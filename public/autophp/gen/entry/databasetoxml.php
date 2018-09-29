665770c2bf3438ce0ff283845d9c7fe5f91f31cc
zhubowen <zhubowen@mujoy.com>
2018-09-29 10:17:29 +0800

    add xml

--- public/autophp/gen/entry/databasetoxml.php
+++ public/autophp/gen/entry/databasetoxml.php
@@ -1,209 +1,154 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
- <HEAD>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <TITLE>代码生成工具</TITLE>
-  <META NAME="Generator" CONTENT="EditPlus">
-  <META NAME="Author" CONTENT="zhuangzhenhua">
-  <META NAME="Keywords" CONTENT="PHP 自动化">
-  <META NAME="Description" CONTENT="">
- </HEAD>
-
- <BODY>
-<!--代码生成开始-->
 <?php
-	error_reporting(E_ALL & ~E_NOTICE &~E_WARNING);
-	ini_set("display_errors", 1);
-	define('BASE_PATH'  , dirname(realpath(dirname(__FILE__))));
-	$autophp_lib = BASE_PATH . "/lib";
-	$include_path[] = ".";
-	$include_path[] = BASE_PATH;
-	$include_path[] = $autophp_lib;
-	set_include_path(implode(PATH_SEPARATOR, $include_path));
+//配置数据库
+header("content-type:text/html;charset=utf-8");
+if($_POST) {
+    $dbserver = isset($_POST["dbserver"])?$_POST["dbserver"]:"localhost";
+    $dbusername = isset($_POST["dbusername"])?$_POST["dbusername"]:"root";
+    $dbpassword = isset($_POST["dbpassword"])?$_POST["dbpassword"]:"";
+    $database = isset($_POST["database"])?$_POST["database"]:"chess";
 
-	$xml = $_GET['xml'];
-	if (!empty($xml))
-		$db_conf_file = BASE_PATH . "/dbxml/{$xml}.xml";
-	else 
-		$db_conf_file = BASE_PATH . "/dbxml/xdwsy.xml";
+    try {
+        $mysql_conn = new PDO("mysql:host=$dbserver;dbname=$database", $dbusername, $dbpassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $mysql_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $mysql_conn->prepare("select * from information_schema.tables where table_schema='{$database}' and table_type='base table'");
+        $stmt->execute();
 
-	require_once 'Project.php';
-	$dst_path = $_POST['proj_path'];
-	$project = new Project($db_conf_file);
-	$project->setDstPath($dst_path);
+        // 设置结果集为关联数组
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach ($stmt->fetchAll() as $k => $v) {
+            // $table = isset($v["Tables_in_" . $database]) ? $v["Tables_in_" . $database] : "";
+            $tablename = $v["TABLE_NAME"];
+            $engine = isset($v["ENGINE"]) ? $v["ENGINE"] : "InnoDB";
+            $tables[$k]['TABLE_NAME'] = $tablename;
+            $tables[$k]['ENGINE'] = $engine;
+        }
+        $no_show_table = array();    //不需要显示的表
+        $no_show_field = array();   //不需要显示的字段
+//循环取得所有表的备注及表中列消息
+        foreach ($tables as $k => $v) {
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.TABLES ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['TABLE_NAME']}'  AND table_schema = '{$database}'";
+            $table_result = $mysql_conn->prepare($sql);
+            $table_result->execute();
+            while ($t = $table_result->fetch(PDO::FETCH_ASSOC)) {
+                $tables[$k]['TABLE_COMMENT'] = $t['TABLE_COMMENT'];
+            }
 
-	$pname = "{$project->getName()}";
-	$pname = "{$_SERVER['SERVER_NAME']}";
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['TABLE_NAME']}' AND table_schema = '{$database}'";
 
-	$project_path = "/data/web/yaf/{$pname}";
-?>
+            $fields = array();
+            $field_result = $mysql_conn->prepare($sql);
+            $field_result->execute();
+            while ($t = $field_result->fetch(PDO::FETCH_ASSOC)) {
+                $fields[] = $t;
+            }
+            $tables[$k]['COLUMN'] = $fields;
+        }
 
-	<div id="div_gen">
-	<div>
-	<pre>
-<?php echo "1、当前配置文件：{$db_conf_file},  如需指定文件,可以指定URL参数（如：?xml=dc)。\n";
-	echo "2、新项目按键按序号执行。\n";
-	echo "3、执行完成后设置hosts: 
-	192.168.99.100 {$pname} #windows
-	127.0.0.1	{$pname} #mac";
-?>
-</pre></div>
-	<form method="post" action="">
-	<li>项目目录: <input type="text" name="proj_path" value="<?php echo $project_path  ?>" style="width:200px"/></li>
-		<table>
-			<tr>
-				<td>table列表</td><td>module列表</td>
-			</tr>
-			<tr>
-			<td valign="top">
+        $xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+        $xml .= "    <database name=\"{$database}\"  shortname=\"{$database}\" user=\"{$dbusername}\" password=\"123456\" hosts=\"%\" title=\"{$database}\" namespace=\"{$database}\">\n";
+        foreach ($tables as $k => $v) {
+            // $html .= '    <h3>' . ($k + 1) . '、' . $v['TABLE_COMMENT'] .'  （'. $v['TABLE_NAME']. '）</h3>'."\n";
+            $commentshow = $v['TABLE_COMMENT']?$v['TABLE_COMMENT']:$v['TABLE_NAME'];
+            $xmlisshow = '       <table name="' . $v['TABLE_NAME'] . '" engine="' . $v['ENGINE'] . '" charset="utf8" autoIncrementStartFrom="1" enableCache="false" isDict="true" comment="' . $commentshow . '" group="">' . "\n";
+            $xmlisshow .= '            <columns>' . "\n";
+            $thispk = "";
+            foreach ($v['COLUMN'] as $f) {
+                if (@!is_array($no_show_field[$v['TABLE_NAME']])) {
+                    $no_show_field[$v['TABLE_NAME']] = array();
+                }
+                if (!in_array($f['COLUMN_NAME'], $no_show_field[$v['TABLE_NAME']])) {
+                    $extra = "";
+                    if($f['COLUMN_KEY']=='PRI'){$thispk = $f['COLUMN_NAME'];}
 
+                    if ($f['EXTRA'] == 'auto_increment') {
+                        $extra = 'extra="auto_increment"';
+                    }else{
+                        $extra = $f['COLUMN_DEFAULT']==""?"":'default="' . $f['COLUMN_DEFAULT'] . '"';
+                    }
+                    $isnull = $f['IS_NULLABLE'] == "NO" ? "false" : "true";
+                    $columncomment = $f['COLUMN_COMMENT']?$f['COLUMN_COMMENT']:$f['COLUMN_NAME'];
+                    $xmlisshow .= '               <column name="' . $f['COLUMN_NAME'] . '" ' . $extra . ' type="' . $f['COLUMN_TYPE'] . '" nullable="' . $isnull . '"  displayName="' .$columncomment . '" displayType="text" />';
+                    $xmlisshow .= "\n";
+                }
+            }
 
+            $xmlisshow .= '        </columns>'."\n";
+            $xmlisshow .= '        <keys>
+			        <key type="pk">
+				        <column name="' . $thispk . '"/>
+		            </key>
+		       </keys>' . "\n";
+            $xmlisshow .= "\n";
+            $xmlisshow .= '    </table>' . "\n";
+            if($thispk) {
+                $xml .=$xmlisshow;
+            }
+        }
+        $xml .= "   </database>\n";
+        unset($_POST);
+        if ($xml) {
+            $file_content = $xml;
+            define('BASE_PATH'  , dirname(realpath(dirname(__FILE__))));
+            $db_conf_file = BASE_PATH . "/dbxml/{$database}.xml";
+            $file_path = $db_conf_file;
+            if (!$fh = fopen($file_path, "w")) {
+                die("ERROR: Open file '$file_path' failed \n");
+            }
 
-<?php	
-	$table = $_POST['table'];
-	$module = $_POST['module'];
-
-	$tables = $project->tables();
-	foreach ($tables as $table_name) {
-		if (in_array($table_name, $table)) {
-			echo '<input type="checkbox" name="table[]" id="' . $table_name . '" value="' . $table_name . '" checked /><label for="' . $table_name . '" style="color:red">' . $table_name . '</label><br>';
-		}
-		else {
-			echo '<input type="checkbox" name="table[]" id="' . $table_name . '" value="' . $table_name . '" checked/><label for="' . $table_name . '">' . $table_name . '</label><br>';
-		}
-	}
-?>
-			</td>
-			<td valign="top">
-<?php
-	$module_list = array("schema", "model", "controller", "view");
-	foreach ($module_list as $module_name) {
-		if (in_array($module_name, $module)) {
-			echo '<input type="checkbox" name="module[]" id="' . $module_name . '" value="' . $module_name . '" checked /><label for="' . $module_name . '" style="color:red">' . $module_name . '</label><br>';			
-		}
-		else {
-			echo '<input type="checkbox" name="module[]" id="' . $module_name . '" value="' . $module_name . '" checked/><label for="' . $module_name . '">' . $module_name . '</label><br>';
-		}
-	}
-?>
-			</td></tr>
-			<tr><td>
-				<input type="submit" value="1、生成代码（多次执行覆盖）" />
-			</td></tr>
-		<table>
-	</form>
-<?php 
-if (!empty($table) && !empty($module)) {
-	if (!file_exists($dst_path)) {
-		die("ERROR: project path isn't exists! ");
-	}
-
-
-
-	$status = $project->partgen($table, $module);
-
-	if ($status) {
-		echo "生成autophp框架完成，下一步，点击导入数据库";
-	}
-	//$model_gen->gen();
+            if(fwrite($fh, $file_content) === FALSE){
+                die("ERROR: Write file '$file_path' failed \n");
+            }
+            fclose($fh);
+            echo "<a href='/autophp/gen/entry/index.php?xml=$database'>生成{$database}.xml成功 请到页面生成autophp框架!</a>";
+            exit;
+        }
+    }catch(Exception $e){
+        header("Content-type: text/html; charset=utf-8");
+        echo $e->getMessage();
+    }
 }
 ?>
-	</div>
-<!--代码生成结束-->
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>数据库表结构</title>
+    <meta name="generator" content="ThinkDb V1.0" />
+    <meta name="author" content="" />
+    <meta name="copyright" content="2008-2014 Tensent Inc." />
+    <style>
+        body, td, th { font-family: "微软雅黑"; font-size: 14px; }
+        .warp{margin:auto; width:900px;}
+        .warp h3{margin:0px; padding:0px; line-height:30px; margin-top:10px;}
+        table { border-collapse: collapse; border: 1px solid #CCC; background: #efefef; }
+        table th { text-align: left; font-weight: bold; height: 26px; line-height: 26px; font-size: 14px; text-align:center; border: 1px solid #CCC; padding:5px;}
+        table td { height: 20px; font-size: 14px; border: 1px solid #CCC; background-color: #fff; padding:5px;}
+        .c1 { width: 120px; }
+        .c2 { width: 120px; }
+        .c3 { width: 150px; }
+        .c4 { width: 80px; text-align:center;}
+        .c5 { width: 80px; text-align:center;}
+        .c6 { width: 80px; }
+        .c7 { width: 190px; }
+    </style>
+</head>
+<body>
+<div class="warp">
+    <h1 style="text-align:center;">数据库表生成xml</h1>
+    <form method="post">
+        数据库地址:<input type="text" name="dbserver" value="192.168.99.100" />
+        数据库名:<input type="text" name="database" value="" />
+        用户名:<input type="text" name="dbusername" value="root" />
+        密码:<input type="text" name="dbpassword" value="123" />
+        <input type="submit" value="生成XML文件" />
+    </form>
 
-<!--数据库导入开始-->
-	<div id="div_import">
-	<form method="post" action="">
-		<table>
-			<tr>
-				<td>项目目录：</td>
-				<td><input type="text" name="proj_path" value="<?php echo $project_path ?>" style="width:200px"/></td>
-			</tr>
-			<tr>
-				<td>DB主机：</td>
-				<td><input type="text" value="mysql" name="db_host"></td>
-			</tr>
-			<tr>
-				<td>DB用名名：</td>
-				<td><input type="text" value="root" name="db_username"></td>
-			</tr>
-			<tr>
-				<td>DB密码：</td>
-				<td><input type="password" value="123456" name="db_password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="checkbox" name="db_fk" id="db_fk" value="1"><label for="db_fk" style="font-size:12px">是否关链外键</label>
-				</td>
-			</tr>
-			<tr><td>
-				<input type="submit" value="2、导入数据库" />
-			</td></tr>
-		</table>
-	</form>
-<?php
-	$db_username = $_POST['db_username'];
-	$db_password = $_POST['db_password'];
-	$db_host = $_POST['db_host'];
-	$db_fk = $_POST['db_fk'];
-
-	if (!empty($db_username)) {
-		$status = $project->importdb($db_username, $db_password, $db_host, $db_fk);
-
-		if ($status) {
-			echo "导入成功, 点击进入<a href='/autophp'>autophp浏览</a>";
-		}
-	}
-?>
-	</div>
-<!--数据库导入结束-->
-
-<!--项目服务，框架搭建开始-->
-<!--
-	<div id="div_server">
-		<form aciton="" method="post">
-			<table>
-			<tr>
-				<td>项目域名：</td>
-				<td><input type="text" name="domain" value="<?php echo $pname ?>"></td>
-			</tr>
-			<tr>
-				<td>WEB根目录：</td>
-				<td><input type="text" name="web_path" value="/data/apache/www"></td>
-			</tr>
-			<tr>
-				<td>HTTP服务：</td>
-				<td><input type="radio" value="apache" name="http_server" id="apache" /><label for="apache" style="font-size:12px">apache</label>
-					<input type="radio" value="nginx" name="http_server" id="nginx" checked/><label for="nginx" style="font-size:12px">nginx</label>
-					安装目录：<input type="text" value="/usr/local/nginx" name="server_path" />
-				</td>
-			</tr>
-			<tr><td>
-				<input type="submit" value="1、框架初始化（执行1次）" />
-			</td></tr>
-		</table>
-		</form>
-<?php
-	$domain = $_POST['domain'];
-	$web_path = $_POST['web_path'];
-	$http_server = $_POST['http_server'];
-	$server_path = $_POST['server_path'];
-	if (!empty($domain)) {
-		$status = $project->server($domain, $http_server, $server_path, $web_path);
-
-		if ($status) {
-			echo "导入成功";
-		}
-		else {
-			echo "项目域名是必须的";
-		}
-	}
-?>
-	</div>
--->
-<!--项目服务，框架搭建结束-->
-	
- </BODY>
-</HTML>
-
-
+</div>
+</body>
+</html>
