7b17befc2a28aa942047f91aba310126ba7d5720
zhuangzhenhua <zhuangzhenhua@miaoju.cn>
2018-09-29 14:47:57 +0800

    更新 Layout.php

--- public/autophp/gen/lib/Layout.php
+++ public/autophp/gen/lib/Layout.php
@@ -576,6 +576,9 @@ EOF;
 
 		if (!empty($input)) {
 			list($display, $desc) = explode(":",$column['displayName'], 2);
+			if (empty($desc)) {
+			    list($display, $desc) = explode("："",$column['displayName'], 2);
+			}
 
 				if (empty($desc))
 					$li = "<li><label class=\"display_name\">{$display}</label>{$input}</li>";
